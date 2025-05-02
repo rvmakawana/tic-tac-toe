ee82e5fef016753d72a726e3073ba4b414ae663e
Ravi Makawana <rvmakawana@gmail.com>
2025-05-02 16:04:33 -0500

    GameOver And Code Reafctot

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import Log from "./components/Log";
 import Player from "./components/Player";
 import { useState } from "react";
 import { WINNING_COMBINATIONS } from "../winning-combinations";
+import Gameover from "./components/GameOver";
 
-const initialGameBoard = [
+
+const PLAYERS = {
+  X: "player1",
+  O: "player2",
+};
+
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -19,36 +27,61 @@ function derivedActiveplayer(gameTurns){
 }
 
 
+function deriveWinner(gameboard,players){
+  let winner = null;
+
+  for(const combination of WINNING_COMBINATIONS){
+     const firstSquareSymbol = gameboard[combination[0].row][combination[0].column];
+     const secondSquareSymbol = gameboard[combination[1].row][combination[1].column];
+     const thirdSquareSymbol = gameboard[combination[2].row][combination[2].column];
+     console.log(`Checking combination: ${combination.map(c => `(${c.row},${c.col})`).join(', ')} - Symbols: ${firstSquareSymbol}, ${secondSquareSymbol}, ${thirdSquareSymbol}`);
+     if(firstSquareSymbol &&
+        firstSquareSymbol === secondSquareSymbol && 
+        firstSquareSymbol ===  thirdSquareSymbol){
+         console.log("Somebody won");
+         winner = players[firstSquareSymbol];
+         break;
+     }
+ 
+  }
+  return winner;
+}
+
+function deriveGameBoard(gameTurns){
+  let gameboard = [...INITIAL_GAME_BOARD.map(array => [...array])];
+  for(const turn of gameTurns){
+      const {square,player} = turn;
+      const {row,col} = square;
+      gameboard[row][col] = player;
+  }
+  return gameboard;
+}
+
+
 function App() {
   
- 
+ const[players,setPlayers] = useState(PLAYERS);
+
+ function handlePlayernameChange(symbol,newName){
+  setPlayers(prevPlayer=> {
+    return {...prevPlayer,[symbol]:newName} 
+  });
+ }
  const[gameTurns,setGameTurns]=useState([]);
- //const[hasWinner,setHasWinner]=useState(false);
+ 
 
  const activePlayer = derivedActiveplayer(gameTurns);
 
+const gameboard = deriveGameBoard(gameTurns);
 
- let gameboard = [...initialGameBoard.map(array => [...array])];
- for(const turn of gameTurns){
-     const {square,player} = turn;
-     const {row,col} = square;
-     gameboard[row][col] = player;
- }
  
- let winner = null;
+const winner = deriveWinner(gameboard,players);
 
- for(const combination of WINNING_COMBINATIONS){
-    const firstSquareSymbol = gameboard[combination[0].row][combination[0].column];
-    const secondSquareSymbol = gameboard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol = gameboard[combination[2].row][combination[2].column];
-    console.log(`Checking combination: ${combination.map(c => `(${c.row},${c.col})`).join(', ')} - Symbols: ${firstSquareSymbol}, ${secondSquareSymbol}, ${thirdSquareSymbol}`);
-    if(firstSquareSymbol &&
-       firstSquareSymbol === secondSquareSymbol && 
-       firstSquareSymbol ===  thirdSquareSymbol){
-        console.log("Somebody won");
-        winner = firstSquareSymbol;
-        break;
-    }
+const hasDraw = gameTurns.length
+ === 9 && winner === null;
+
+ function handleRestart(){
+    setGameTurns([]);
 
  }
 
@@ -67,10 +100,10 @@ function App() {
     <main>
       <div id = "game-container">
         <ol id= "players" className="highlight-player">
-          <Player initialName = "Player 1" symbol = "X" isActive={activePlayer === 'X'}/>
-          <Player  initialName = "Player 2" symbol = "O" isActive={activePlayer === 'O'}/>
+          <Player initialName = {PLAYERS.X} symbol = "X" isActive={activePlayer === 'X'}  onChangeName={handlePlayernameChange} />
+          <Player  initialName = {PLAYERS.O} symbol = "O" isActive={activePlayer === 'O'} onChangeName={handlePlayernameChange}  />
         </ol>
-        {winner && <p>You Won , {winner}!</p> }
+        {(winner || hasDraw) && <Gameover winner={winner} onRestart={handleRestart}/> }
        <GameBoard onSelectSquare={handleSelectSquare} board = {gameboard}/>
       </div>
       <Log turns={gameTurns} />
